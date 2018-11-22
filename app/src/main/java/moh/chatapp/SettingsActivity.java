68187439d4fceb0f64876f526fba3e4c0d381651
mOh <mh19.my@gmail.com>
2018-11-22 23:38:48 +0200

    change status

--- app/src/main/java/moh/chatapp/SettingsActivity.java
+++ app/src/main/java/moh/chatapp/SettingsActivity.java
@@ -1,8 +1,11 @@
 package moh.chatapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +25,9 @@ public class SettingsActivity extends AppCompatActivity {
     private CircleImageView mDisplayImage;
     private TextView mName;
     private TextView mStatus;
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +36,8 @@ public class SettingsActivity extends AppCompatActivity {
         mDisplayImage = findViewById(R.id.settings_image);
         mStatus = findViewById(R.id.settings_status);
         mName = findViewById(R.id.settings_display_name);
-
+        mStatusBtn = findViewById(R.id.settings_status_btn);
+        mImageBtn = findViewById(R.id.settings_image_btn);
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
         String currentUID = mCurrentUser.getUid();
         mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUID);
@@ -49,6 +56,17 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String status_value = mStatus.getText().toString();
+                Intent mainIntent = new Intent(SettingsActivity.this,StatusActivity.class);
+                mainIntent.putExtra("status value", status_value);
+                startActivity(mainIntent);
             }
         });
     }
