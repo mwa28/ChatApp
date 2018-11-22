c3932d704450fabd2e4ffe105bd3a33f7a12e1b9
mOh <mh19.my@gmail.com>
2018-11-22 19:20:05 +0200

    user registration and database entry

--- app/src/main/java/moh/chatapp/RegisterActivity.java
+++ app/src/main/java/moh/chatapp/RegisterActivity.java
@@ -7,9 +7,12 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,63 +20,136 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.Objects;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "EmailPassword";
     private TextInputLayout mDisplayName;
-    private TextInputLayout mEmail;
-    private TextInputLayout mPassword;
-    private Button mCreateBtn;
+    private EditText mEmail;
+    private EditText mPassword;
 
     private FirebaseAuth mAuth;
     private Toolbar mToolarBar;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mToolarBar = (Toolbar) findViewById(R.id.register_toolbar);
+        mToolarBar = findViewById(R.id.register_toolbar);
         setSupportActionBar(mToolarBar);
         getSupportActionBar().setTitle("Create an Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mDisplayName = findViewById(R.id.reg_display_name);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+
+        findViewById(R.id.reg_create_btn).setOnClickListener(this);
+
         mAuth = FirebaseAuth.getInstance();
-
-        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
-        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
-        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
-        mCreateBtn = (Button) findViewById(R.id.reg_create_btn);
-
-        mCreateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String display_name = mDisplayName.getEditText().getText().toString();
-                String email = mEmail.getEditText().getText().toString();
-                String password = mPassword.getEditText().getText().toString();
-
-                register_user(display_name,email,password);
-            }
-        });
     }
 
-    private void register_user(String display_name, String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmail.setError("Required.");
+            valid = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        String password = mPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPassword.setError("Required.");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void createAccount(final String display_name, String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String uid = user.getUid();
+                            mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                            HashMap<String, String> userMap = new HashMap<>();
+                            userMap.put("name", display_name);
+                            userMap.put("status", "Hi there! I am using ChatApp.");
+                            userMap.put("image", "default");
+                            userMap.put("thumb_image", "default");
+
+                            mDatabase.setValue(userMap);
+                            updateUI();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI();
+                        }
+
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void register_user(final String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
+                if (task.isSuccessful()) {
+
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+
+
                 } else {
                     Toast.makeText(RegisterActivity.this, "Error", Toast.LENGTH_LONG).show();
 
                 }
             }
         });
+    }
+
+    private void updateUI(){
+        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.reg_create_btn) {
+            createAccount(mDisplayName.getEditText().getText().toString(),mEmail.getText().toString(), mPassword.getText().toString());
+        }
     }
 }
